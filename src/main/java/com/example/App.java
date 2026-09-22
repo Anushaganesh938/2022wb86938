109eafea611f8d977edf9216ea9e29d61217318c
Anushaganesh938 <2022wb86938@wilp.bits-pilani.ac.in>
2026-09-23 01:39:36 +0530

    Update output message for Jenkins CI

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from 2022wb86938 - DevOps Continuous Integration");
+        System.out.println("Hello from 2022wb86938 - Jenkins CI update");
     }
 }
